32b6e05402143d27698726edf3966d48b7469756
Mayank Saxena <sm.mayank@icloud.com>
2017-04-06 19:38:53 +0200

    redis handling port for prod

--- api/services/Redis.js
+++ api/services/Redis.js
@@ -1,5 +1,6 @@
 var redis = require('redis');
-var redisClient = redis.createClient();
+const redisUrl = process.env.REDIS_URL | '127.0.0.1:6379';
+var redisClient = redis.createClient(redisUrl);
 
 redisClient.on('error', function(err) {
   sails.log.error(err);
