fa26213eff58c6d0db1389a91c41f939817aacaa
Mayank Saxena <sm.mayank@icloud.com>
2017-04-07 16:23:51 +0200

    Redis localhost url schema

--- api/services/Redis.js
+++ api/services/Redis.js
@@ -1,5 +1,5 @@
 var redis = require('redis');
-const redisUrl = process.env.REDIS_URL || '127.0.0.1:6379';
+const redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 var redisClient = redis.createClient(redisUrl);
 
 redisClient.on('error', function(err) {
